569458c8c16b8465b80c93eac62bee5963f636a9
Ishwor <ishwortimalsina39@gmail.com>
2023-03-08 22:44:10 +0545

    more things are added in cpp

--- recursion.cpp
+++ recursion.cpp
@@ -2,8 +2,20 @@
 #include <iostream>
 using namespace std;
 
+int factorialFinder(int x)
+{
+    if (x == 1 || x == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return x * factorialFinder(x - 1);
+    }
+}
+
 int main()
 {
-    cout << x << endl;
+    cout << "The factorial using recursion is: " << factorialFinder(5) << endl;
     return 0;
 }
