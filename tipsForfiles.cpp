5d03ad83f3f0c32ddbf87943aabc45139f261cad
Ishwor <ishwortimalsina39@gmail.com>
2023-03-13 22:45:46 +0545

    missed one

---
+++ tipsForfiles.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream> //file's headers file
+using namespace std;
+
+int main()
+{
+
+    ofstream iceFile("fire1.txt");
+
+    if (iceFile.is_open())
+    {
+        cout << "okay the file is open" << endl;
+    }
+    else
+    {
+        cout << "you messed up" << endl;
+    }
+
+    iceFile << "I don't love fire but fire loves me! \n";
+    iceFile.close();
+    return 0;
+}
